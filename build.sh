d2921619cd26342f65a2a9b49fbeae282dc53793
Per Abich <per.abich@gmail.com>
2015-11-05 08:43:10 +0000

    Improved build.sh script

---
+++ build.sh
@@ -0,0 +1,15 @@
+
+if [[ -z $GOPATH ]]
+then
+    export GOPATH=$HOME/go
+    mkdir -p $GOPATH
+fi
+if [[ -z $(which gb) ]]
+then
+    go get github.com/constabulary/gb/...
+    go clean
+    go install -v github.com/constabulary/gb/
+    sudo cp $GOPATH/bin/gb* /usr/local/bin
+fi
+
+gb build all
